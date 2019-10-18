b8f5c681cdce60d329e4f2c365ec00a9a52d3130
MinexAutomation <minex.automation@gmail.com>
2019-10-18 15:12:09 -0700

    Updated field-name.

--- source/R5T.Frisia.Base/Code/Classes/AwsEc2ServerSecrets.cs
+++ source/R5T.Frisia.Base/Code/Classes/AwsEc2ServerSecrets.cs
@@ -5,7 +5,7 @@ namespace R5T.Frisia
 {
     public class AwsEc2ServerSecrets
     {
-        public string Host { get; set; }
+        public string HostUrl { get; set; }
         public string UserID { get; set; }
         public string Password { get; set; }
         public string PrivateKeyFilePath { get; set; }
