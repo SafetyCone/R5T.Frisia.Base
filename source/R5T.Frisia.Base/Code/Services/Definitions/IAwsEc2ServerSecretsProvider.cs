cb1795b7a517c11a979f0de2d5e3520aad7eb68c
davidcoats <david@getsafetycone.com>
2022-04-27 15:31:06 -0700

    Interim changes.

--- source/R5T.Frisia.Base/Code/Services/Definitions/IAwsEc2ServerSecretsProvider.cs
+++ source/R5T.Frisia.Base/Code/Services/Definitions/IAwsEc2ServerSecretsProvider.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
+using R5T.T0064;
+
 
 namespace R5T.Frisia
 {
-    public interface IAwsEc2ServerSecretsProvider
+    [ServiceDefinitionMarker]
+    public interface IAwsEc2ServerSecretsProvider : IServiceDefinition
     {
         AwsEc2ServerSecrets GetAwsEc2ServerSecrets();
     }
